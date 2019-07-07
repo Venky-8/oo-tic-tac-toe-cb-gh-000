63edb1787ce5ee7e5c6899d80df50782210266ec
Venkatesh Mangnale <mangnalev.1999@gmail.com>
2019-07-07 10:31:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,25 +21,25 @@ class TicTacToe
     [2,4,6]
   ]
 
-  def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
   def input_to_index(input)
     input.to_i - 1
   end
 
-  def move(board, index, token)
-    board[index] = token
+  def move(index, token)
+    @board[index] = token
   end
 
-  def valid_move?(board, index)
+  def valid_move?(index)
     if index >= 0 && index <= 8
-      value = board[index]
+      value = @board[index]
       if value == "" || value == " " || value == nil
         true
       end
@@ -48,22 +48,22 @@ class TicTacToe
     end
   end
 
-  def turn(board)
+  def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
+    if valid_move?(index)
+      move(index, current_player)
     else
       puts "Enter a valid move"
-      turn(board)
+      turn
     end
-    display_board(board)
+    display_board
   end
 
-  def turn_count(board)
+  def turn_count
     counter = 0
-    board.each do |token|
+    @board.each do |token|
       if token == 'X' || token == 'O'
         counter+=1
       end
@@ -71,51 +71,52 @@ class TicTacToe
     return counter
   end
 
-  def current_player(board)
-    if turn_count(board).even?
+  def current_player
+    if turn_count.even?
       'X'
     else
       'O'
     end
   end
 
-  def won?(board)
+  def won?
     WIN_COMBINATIONS.find do |indexes|
       ["X", "O"].any? do |c|
-        indexes.all?{|i| board[i]==c}
+        indexes.all?{|i| @board[i]==c}
       end
     end
   end
 
-  def full?(board)
-    board.none?{|c| c == " "}
+  def full?
+    @board.none?{|c| c == " "}
   end
 
-  def draw?(board)
-    full?(board) and won?(board) == nil
+  def draw?
+    full? and won? == nil
   end
 
-  def over?(board)
-    won?(board) or draw?(board) or full?(board)
+  def over?
+    won? or draw? or full?
   end
 
-  def winner(board)
-    winner = won?(board)
+  def winner
+    winner = won?
     if winner == nil
       nil
     else
-      board[winner[0]]
+      @board[winner[0]]
     end
   end
 
-  def play(board)
-    while !over?(board)
-      turn(board)
+  def play
+    while !over?
+      turn
     end
-    if winner(board) != nil
+    if winner != nil
       puts "Congratulations #{winner(board)}!"
     else
       puts "Cat's Game!"
     end
   end
+  
 end
